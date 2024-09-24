cb61f53b609037ba9cc791677ddaeb7e038ca956
IT22025180 <it22025180@my.sliit.lk>
2024-09-24 18:13:34 +0530

    vanuja

--- frontend/form.py
+++ frontend/form.py
@@ -1,6 +1,20 @@
 import streamlit as st
 import numpy as np
 from urllib.parse import urlparse,parse_qs
+import joblib
+
+def load_model():
+    try:
+        return joblib.load('classification_model.pkl')
+    except FileNotFoundError:
+        st.error("Model file not found. Please ensure 'random_forest_model.pkl' is in the correct directory.")
+        st.stop()
+    except Exception as e:
+        st.error(f"An error occurred while loading the model: {e}")
+        st.stop()
+
+# Load the model
+model = load_model()
 
 def get_query_params():
     
@@ -21,144 +35,186 @@ def display_page():
         import result
 
 def user_input_form():
-    Age = st.selectbox(
-        'Choose your age:',
-        ['18', '19', '20', '21', '22','23','24','25','26','27','28',
-        '29','30','31','32','33','34','35','36','37','38',
-        '39','40','41','42','43','44','45','46','47','48',
-        '49','50','51','52','53','54','55','56','57','58','59','60']
-    )
-    st.text(Age)
+    with st.form("input_form"):
+        st.title("Employee Attrition Predictor")
+        Age = st.selectbox(
+            'Choose your age:',
+            ['18', '19', '20', '21', '22','23','24','25','26','27','28',
+            '29','30','31','32','33','34','35','36','37','38',
+            '39','40','41','42','43','44','45','46','47','48',
+            '49','50','51','52','53','54','55','56','57','58','59','60']
+        )
+        st.text(Age)
 
-    DailyRate = st.number_input(
-        'Enter your daily rate:',
-        min_value=0,      # Minimum value allowed
-        key='dailyRate'
-    )
-    st.text(DailyRate)
+        DailyRate = st.number_input(
+            'Enter your daily rate:',
+            min_value=0,      # Minimum value allowed
+            key='dailyRate'
+        )
+        st.text(DailyRate)
 
-    Department = st.selectbox(
-        'Choose your department:',
-        ['Sales', 'Research & Development', 'Human Resources']
-    )
-    st.text(Department)
+        Department = st.selectbox(
+            'Choose your department:',
+            ['2', '1', '0']
+        )
+        st.text(Department)
 
-    DistanceFromHome = st.number_input(
-        'Enter distance in KM:',
-        min_value=0,      # Minimum value allowed
-        max_value=100,    # Maximum value allowed
-        step=1,            # Step size
-        key='distfromhome'
-    )
-    st.text(DistanceFromHome)
 
-    EnvironmentSatisfaction = st.selectbox(
-        'Choose your environment satisfaction:',
-        ['4', '3', '2', '1']
-    )
-    st.text(EnvironmentSatisfaction)
+        DistanceFromHome = st.number_input(
+            'Enter distance in KM:',
+            min_value=0,      # Minimum value allowed
+            max_value=100,    # Maximum value allowed
+            step=1,            # Step size
+            key='distfromhome'
+        )
+        st.text(DistanceFromHome)
 
-    Gender = st.selectbox(
-        'Gender:',
-        ['Male', 'Female']
-    )
-    st.text(Gender)
+        EnvironmentSatisfaction = st.selectbox(
+            'Choose your environment satisfaction:',
+            ['4', '3', '2', '1']
+        )
+        st.text(EnvironmentSatisfaction)
 
-    JobLevel = st.selectbox(
-        'Choose your job level:',
-        ['5', '4', '3', '2', '1']
-    )
-    st.text(JobLevel)
+        Gender = st.selectbox(
+            'Gender:',
+            ['1', '0']
+        )
+        st.text(Gender)
 
-    JobRole = st.selectbox(
-        'Choose your job role:',
-        ['Sales Executive', 'Research Scientist', 'Laboratory Technician', 'Manufacturing Director', 'Healthcare Representative','Manager','Sales Representative','Research Director','Human Resources']
-    )
-    st.text(JobRole)
+        JobLevel = st.selectbox(
+            'Choose your job level:',
+            ['5', '4', '3', '2', '1']
+        )
+        st.text(JobLevel)
 
-    JobSatisfaction = st.selectbox(
-        'Choose your job satisfaction:',
-        ['4', '3', '2', '1']
-    )
-    st.text(JobSatisfaction)
+        JobRole = st.selectbox(
+            'Choose your job role:',
+            ['0', '1', '2', '3', '4','5','6','7','8']
+        )
+        st.text(JobRole)
 
-    MaritalStatus = st.selectbox(
-        'Choose your marital status:',
-        ['Single', 'Married', 'Divorced']
-    )
-    st.text(MaritalStatus)
+        JobSatisfaction = st.selectbox(
+            'Choose your job satisfaction:',
+            ['4', '3', '2', '1']
+        )
+        st.text(JobSatisfaction)
 
-    MonthlyIncome = st.number_input(
-        'Enter your monthly income:',
-        min_value=0,      # Minimum value allowed
-        key = 'monthlyincome'
-    )
-    st.text(MonthlyIncome)
+        MaritalStatus = st.selectbox(
+            'Choose your marital status:',
+            ['2', '1', '0']
+        )
+        st.text(MaritalStatus)
 
-    MonthlyRate = st.number_input(
-        'Enter your monthly rate:',
-        min_value=0,      # Minimum value allowed
-        key = 'monthlyrate'
-    )
-    st.text(MonthlyRate)
+        MonthlyIncome = st.number_input(
+            'Enter your monthly income:',
+            min_value=0,      # Minimum value allowed
+            key = 'monthlyincome'
+        )
+        st.text(MonthlyIncome)
 
-    NumCompaniesWorked = st.selectbox(
-        'Number of companies work with:',
-        ['1', '2', '3', '4', '5','6','7','8','9','10']
-    )
-    st.text(NumCompaniesWorked)
+        MonthlyRate = st.number_input(
+            'Enter your monthly rate:',
+            min_value=0,      # Minimum value allowed
+            key = 'monthlyrate'
+        )
+        st.text(MonthlyRate)
 
-    OverTime = st.selectbox(
-        'Choose your over time:',
-        ['Yes', 'No']
-    )
-    st.text(OverTime)
+        NumCompaniesWorked = st.selectbox(
+            'Number of companies work with:',
+            ['1', '2', '3', '4', '5','6','7','8','9','10']
+        )
+        st.text(NumCompaniesWorked)
 
-    TotalWorkingYears = st.number_input(
-        'Enter distance in KM:',
-        min_value=0,      # Minimum value allowed
-        max_value=20,    # Maximum value allowed
-        step=1,            # Step size
-        key='totalworkyrs'
-    )
-    st.text(TotalWorkingYears)
+        OverTime = st.selectbox(
+            'Choose your over time:',
+            ['1', '0']
+        )
+        st.text(OverTime)
 
-    WorkLifeBalance = st.selectbox(
-        'Choose your work life balance:',
-        ['4', '3', '2', '1']
-    )
-    st.text(WorkLifeBalance)
+        TotalWorkingYears = st.number_input(
+            'Total working years:',
+            min_value=0,      # Minimum value allowed
+            max_value=20,    # Maximum value allowed
+            step=1,            # Step size
+            key='totalworkyrs'
+        )
+        st.text(TotalWorkingYears)
 
-    YearsAtCompany = st.number_input(
-        'Years at company:',
-        min_value=0,      # Minimum value allowed
-        max_value=20,    # Maximum value allowed
-        step=1,         # Step size
-        key='yrsAtcompany'
-    )
-    st.text(YearsAtCompany)
+        WorkLifeBalance = st.selectbox(
+            'Choose your work life balance:',
+            ['4', '3', '2', '1']
+        )
+        st.text(WorkLifeBalance)
 
-    YearsInCurrentRole = st.selectbox(
-        'Choose your years in current role:',
-        ['1', '2', '3', '4', '5','6','7','8','9','10',
-        '11','12','13','14','15','16','17','18','19','20']
-    )
-    st.text(YearsInCurrentRole)
+        YearsAtCompany = st.number_input(
+            'Years at company:',
+            min_value=0,      # Minimum value allowed
+            max_value=20,    # Maximum value allowed
+            step=1,         # Step size
+            key='yrsAtcompany'
+        )
+        st.text(YearsAtCompany)
 
-    YearsSinceLastPromotion = st.selectbox(
-        'Choose your year since last promotion:',
-        ['0', '1', '2', '3', '4','5','6','7','8','9','10','11','12','13','14','15']
-    )
-    st.text(YearsSinceLastPromotion)
+        YearsInCurrentRole = st.selectbox(
+            'Choose your years in current role:',
+            ['1', '2', '3', '4', '5','6','7','8','9','10',
+            '11','12','13','14','15','16','17','18','19','20']
+        )
+        st.text(YearsInCurrentRole)
 
-    inputs = {
-        'Age': Age, 'DailyRate': DailyRate, 'Department': Department, 'DistanceFromHome': DistanceFromHome,
-        'EnvironmentSatisfaction': EnvironmentSatisfaction, 'Gender': Gender, 'JobLevel': JobLevel, 'JobRole': JobRole,
-        'JobSatisfaction': JobSatisfaction, 'MaritalStatus': MaritalStatus,'MonthlyIncome': MonthlyIncome, 'MonthlyRate': MonthlyRate,
-        'NumCompaniesWorked': NumCompaniesWorked, 'OverTime': OverTime, 'TotalWorkingYears': TotalWorkingYears, 'WorkLifeBalance': WorkLifeBalance,
-        'YearsAtCompany': YearsAtCompany, 'YearsInCurrentRole': YearsInCurrentRole, 'YearsSinceLastPromotion': YearsSinceLastPromotion
-    }
+        YearsSinceLastPromotion = st.selectbox(
+            'Choose your year since last promotion:',
+            ['0', '1', '2', '3', '4','5','6','7','8','9','10','11','12','13','14','15']
+        )
+        st.text(YearsSinceLastPromotion)
 
-    return inputs
+        submitted = st.form_submit_button("Predict")
 
-display_page()
+    if submitted:
+
+        input_data = {
+            'Age': Age, 'DailyRate': DailyRate, 'Department': Department, 'DistanceFromHome': DistanceFromHome,
+            'EnvironmentSatisfaction': EnvironmentSatisfaction, 'Gender': Gender, 'JobLevel': JobLevel, 'JobRole': JobRole,
+            'JobSatisfaction': JobSatisfaction, 'MaritalStatus': MaritalStatus,'MonthlyIncome': MonthlyIncome, 'MonthlyRate': MonthlyRate,
+            'NumCompaniesWorked': NumCompaniesWorked, 'OverTime': OverTime, 'TotalWorkingYears': TotalWorkingYears, 'WorkLifeBalance': WorkLifeBalance,
+            'YearsAtCompany': YearsAtCompany, 'YearsInCurrentRole': YearsInCurrentRole, 'YearsSinceLastPromotion': YearsSinceLastPromotion
+        }
+
+        st.session_state['input_data'] = input_data
+
+# Function to display prediction results
+def display_result():
+    input_data = st.session_state.get('input_data', None)
+    
+    if input_data:
+        # Convert the inputs to a NumPy array
+        data_to_predict = np.array([
+            input_data['Age'], input_data['DailyRate'], input_data['Department'], input_data['DistanceFromHome'],
+            input_data['EnvironmentSatisfaction'], input_data['Gender'], input_data['JobLevel'], input_data['JobRole'],
+            input_data['JobSatisfaction'], input_data['MaritalStatus'], input_data['MonthlyIncome'], input_data['MonthlyRate'],
+            input_data['NumCompaniesWorked'], input_data['OverTime'], input_data['TotalWorkingYears'],
+            input_data['WorkLifeBalance'], input_data['YearsAtCompany'], input_data['YearsInCurrentRole'],
+            input_data['YearsSinceLastPromotion']
+        ]).reshape(1, -1)
+        
+        # Prediction
+        prediction = model.predict(data_to_predict)[0]
+        
+        # Display result
+        if prediction == 1:
+            st.success('This employee is likely to leave.')
+        else:
+            st.success('This employee is likely to stay.')
+    else:
+        st.warning('Please fill out the form first.')
+
+
+def main():
+    if 'input_data' in st.session_state:
+        display_result()
+    else:
+        user_input_form()
+
+# Run the app
+if __name__ == '__main__':
+    main()
